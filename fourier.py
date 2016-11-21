1fcab71736610e0f2cde93afabf3bbdf18a3b2c0
Tim Van Slyke <vanslyke.t@husky.neu.edu>
2016-11-21 12:09:41 -0500

    Improved things because I knew somebody would be looking at this in the near future.

--- fourier.py
+++ fourier.py
@@ -1,23 +1,40 @@
-from math import sin, cos, sqrt, pi
-from scipy.integrate import quad 
+""" Module exploiting Python's functional programming features to demonstrate
+    that the Fourier series is actually just a vector projection.
+"""
 
+from math import sin, cos, pi
+from scipy.integrate import quad
 
-def fourierInnerProduct(func1, func2, integrator = quad):
+SQRT_PI = pi ** .5
+
+def quad_integrator(func, lower_bound, upper_bound):
+    start, end = quad(func, lower_bound, upper_bound)
+    return end - start
+    
+
+def fourierInnerProduct(func1, func2, integrator = quad_integrator):
     """ Inner product for the Fourier series.
-        func1, func2: functions upon which the product will be computed.
-    """
-    # integrate the product of func1 and func2 over -pi to pi
-    start, end = integrator(lambda x: func1(x) * func2(x), -pi, pi)
+        Integral from -pi to pi of ((func1(t) * func2(t))dt) / pi
 
-    # return 
-    return (end - start)/pi
+        Arguments:
+        func1, func2:   Functions upon which the product will be computed.
+        integrator:     Function that accepts a function, a lowerbound, and
+                        an upper bound and returns the integral of that function
+                        from it's lower bound to it's upper bound (one number scalar).
+                        integrator(func: function, lower_bound: float, upper_bound: float)->float
+                        
+    """
+    # integrate the product of func1 and func2 over -pi to pi and return
+    return integrator(lambda x: func1(x) * func2(x), -pi, pi)/pi
 
 def project(function, basis, innerProductDef):
     """ Project 'function' onto the vector space with basis 'basis'.
+
+        Arguments:
         function: function to be projected onto 'basis'.
         basis: list of functions defining the basis of the vector space.
         innerProductDef: definition of an inner product. Must accept two
-                         functions and a resolution argument.  Should return
+                         functions and should return
                          scalar values according to the definition of the
                          inner product, found here:
                          https://en.wikipedia.org/wiki/Inner_product_space#Definition
@@ -44,16 +61,16 @@ def project(function, basis, innerProductDef):
     return (lambda arg: sum([vec(arg) for vec in projection])), coeffs
 
 
-def fourier(function, order = 10):
+def fourier(function, order = 10, projection = project, innerProductDef = fourierInnerProduct):
     """ Returns a function representing the fourier series for the input.
         function:   function for which the fourier series will be calculated.
         order:      order of the fourier series to be calculated.
                     ex) order = 10 calculates terms up to cos(10t), and sin(10t)
     """
-
+    
     # create a list of basis functions for the vector space of sines and cosines.
     # The first basis vector/function is the constant function, sqrt(pi)
-    basis = [lambda x: sqrt(pi)] + 2 * order * [0]
+    basis = [lambda x: SQRT_PI] + 2 * order * [0]
 
     # iterate through each pair of sinusoids
     for index in xrange(1, order + 1):
@@ -63,7 +80,7 @@ def fourier(function, order = 10):
         basis[2*index] = (lambda x, index=index:  cos(index * x))
 
     # calculate the projection of 'function' onto the generated basis
-    projFunc, coeffs = project(function, basis, fourierInnerProduct)
+    projFunc, coeffs = projection(function, basis, innerProductDef)
 
     # return the projected function and the coefficients of the series
     return projFunc, coeffs
@@ -72,7 +89,7 @@ def fourier(function, order = 10):
     
 if __name__ == '__main__':
     from numpy import linspace
-    from math import exp
+    from math import exp, sqrt
     import matplotlib.pyplot as plt
 
     # define your function here.  may be a lambda such as:
